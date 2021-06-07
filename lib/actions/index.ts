3257d5a55e2b2a22bea6b69b84efad0215a8f2a3
Graham McCulloch <admin@grahammcculloch.co.uk>
2021-06-07 08:43:37 +0700

    Fix TypeScript error
    Change-type: patch
    Signed-off-by: Graham McCulloch <graham@balena.io>

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -4,6 +4,8 @@
  * Proprietary and confidential.
  */
 
+import { ActionFile } from '@balena/jellyfish-plugin-base';
+import { ContractData } from '@balena/jellyfish-types/build/core';
 import { actionMatchMakeTask } from './action-matchmake-task';
 
-export const actions = [actionMatchMakeTask];
+export const actions: Array<ActionFile<ContractData>> = [actionMatchMakeTask];

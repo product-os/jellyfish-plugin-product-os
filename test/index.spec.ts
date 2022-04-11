4a50ab9573ed2a6c3f8b9b200c535dc8e22a06da
Josh Bowling <josh@balena.io>
2022-04-11 14:08:27 +0900

    Remove transformer contracts
    Moved these contracts to jellyfish-worker
    
    Change-type: major
    Signed-off-by: Josh Bowling <josh@balena.io>

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -5,22 +5,11 @@ import { productOsPlugin } from '../lib/index';
 const pluginManager = new PluginManager([productOsPlugin()]);
 
 test('Plugin returns collection of contracts', () => {
-	const cards = pluginManager.getCards();
-
-	expect(_.isEmpty(cards)).toBeFalsy();
+	const contracts = pluginManager.getCards();
+	expect(_.isEmpty(contracts)).toBeFalsy();
 });
 
 test('Expected contracts are loaded', () => {
-	const cards = pluginManager.getCards();
-
-	// Sanity check
-	expect(cards.image.name).toBe('Container image contract');
-	expect(cards['action-matchmake-task'].slug).toBe('action-matchmake-task');
-});
-
-test('Expected actions are loaded', () => {
-	const actions = pluginManager.getActions();
-
-	// Sanity check
-	expect(typeof actions['action-matchmake-task'].handler).toBe('function');
+	const contracts = pluginManager.getCards();
+	expect(contracts['contract-repository'].name).toBe('Repository');
 });

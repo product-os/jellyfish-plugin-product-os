d281b61384be64260af46c2d62dbd4224f7ab676
Carlo Miguel F. Cruz <carloc@balena.io>
2022-07-08 08:36:50 +0000

    Removes transformer relationship contract from productOS plugin.
    Change-type: patch
    Signed-off-by: Carlo Miguel F. Cruz <carloc@balena.io>

--- lib/contracts/index.ts
+++ lib/contracts/index.ts
@@ -5,9 +5,6 @@ import { loopBalenalabs } from './loop-balenalabs';
 import { loopCompanyOs } from './loop-company-os';
 import { loopProductOs } from './loop-product-os';
 import { loopTeamOs } from './loop-team-os';
-import { relationshipContractRepositoryContainsAny } from './relationship-contract-repository-contains-any';
-import { relationshipContractRepositoryLatestAny } from './relationship-contract-repository-latest-any';
-import { relationshipLoopOwnsContractRepository } from './relationship-loop-owns-contract-repository';
 import { roleLoop } from './role-loop';
 import { viewAllJellyfishSupportThreads } from './view-all-jellyfish-support-threads';
 
@@ -20,7 +17,4 @@ export const contracts: Array<ContractDefinition<ContractData>> = [
 	loopTeamOs,
 	roleLoop,
 	viewAllJellyfishSupportThreads,
-	relationshipContractRepositoryContainsAny,
-	relationshipContractRepositoryLatestAny,
-	relationshipLoopOwnsContractRepository,
 ];
--- lib/types/contracts/index.ts
+++ lib/types/contracts/index.ts
@@ -7,4 +7,5 @@
 export type {
 	ContractRepositoryContract,
 	ContractRepositoryContractDefinition,
+	ContractRepositoryData,
 } from './contract-repository';
